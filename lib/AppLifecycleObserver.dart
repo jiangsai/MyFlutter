e361f2318e3eaec77ce7d29faf0793625fe5d173
jiangyaoteng <125839128@qq.com>
2021-12-15 18:04:53 +0800

    first

---
+++ lib/AppLifecycleObserver.dart
@@ -0,0 +1,8 @@
+
+
+
+import 'package:flutter_boost/flutter_boost.dart';
+
+class AppLifecycleObserver extends GlobalPageVisibilityObserver{
+
+}
---
+++ lib/main.dart
@@ -0,0 +1,140 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_boost/flutter_boost.dart';
+
+import 'AppLifecycleObserver.dart';
+
+void main() {
+  ///添加全局生命周期监听类
+
+  PageVisibilityBinding.instance.addGlobalObserver(AppLifecycleObserver());
+
+  ///这里的CustomFlutterBinding调用务必不可缺少，用于控制Boost状态的resume和pause
+  CustomFlutterBinding();
+
+  runApp(const MyApp());
+}
+
+///创建一个自定义的Binding，继承和with的关系如下，里面什么都不用写
+class CustomFlutterBinding extends WidgetsFlutterBinding
+    with BoostFlutterBinding {}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key key}) : super(key: key);
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: '1111',
+      theme: ThemeData(
+        // This is the theme of your application.
+        //
+        // Try running your application with "flutter run". You'll see the
+        // application has a blue toolbar. Then, without quitting the app, try
+        // changing the primarySwatch below to Colors.green and then invoke
+        // "hot reload" (press "r" in the console where you ran "flutter run",
+        // or press Run > Flutter Hot Reload in a Flutter IDE). Notice that the
+        // counter didn't reset back to zero; the application is not restarted.
+        primarySwatch: Colors.blue,
+      ),
+      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({Key key, this.title}) : super(key: key);
+
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  /// 由于很多同学说没有跳转动画，这里是因为之前exmaple里面用的是 [PageRouteBuilder]，
+  /// 其实这里是可以自定义的，和Boost没太多关系，比如我想用类似iOS平台的动画，
+  /// 那么只需要像下面这样写成 [CupertinoPageRoute] 即可
+  /// (这里全写成[MaterialPageRoute]也行，这里只不过用[CupertinoPageRoute]举例子)
+  ///
+  /// 注意，如果需要push的时候，两个页面都需要动的话，
+  /// （就是像iOS native那样，在push的时候，前面一个页面也会向左推一段距离）
+  /// 那么前后两个页面都必须是遵循CupertinoRouteTransitionMixin的路由
+  /// 简单来说，就两个页面都是CupertinoPageRoute就好
+  /// 如果用MaterialPageRoute的话同理
+
+  static Map<String, FlutterBoostRouteFactory> routerMap = {
+    'mainPage': (settings, uniqueId) {
+      return CupertinoPageRoute(
+          settings: settings,
+          builder: (_) {
+            // Map<String, Object> map = settings.arguments;
+            // String data = map['data'];
+            return MainPage();
+          });
+    },
+  };
+
+  Route<dynamic> routeFactory(RouteSettings settings, String uniqueId) {
+    FlutterBoostRouteFactory func =
+        routerMap[settings.name] as FlutterBoostRouteFactory;
+    if (func == null) {
+      return null;
+    }
+    return func(settings, uniqueId);
+  }
+
+  Widget appBuilder(Widget home) {
+    return MaterialApp(
+      home: home,
+      debugShowCheckedModeBanner: true,
+
+      ///必须加上builder参数，否则showDialog等会出问题
+      builder: (_, __) {
+        return home;
+      },
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return FlutterBoostApp(
+      routeFactory,
+      appBuilder: appBuilder,
+    );
+  }
+}
+
+class MainPage extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    return _MainState();
+  }
+}
+
+class _MainState extends State<MainPage> {
+  @override
+  Widget build(BuildContext context) {
+    return const Center(
+      child: MaterialButton(
+        child: Text(
+          "success",
+          style: TextStyle(
+            fontSize: 30.0, // 文字大小
+            color: Colors.yellow, // 文字颜色
+          ),
+        ),
+        onPressed: click,
+      ),
+    );
+  }
+}
+
+click() {
+  BoostNavigator.instance.push(
+    "yourPage", //required
+    withContainer: false, //optional
+    opaque: true, //optional,default value is true
+  );
+}
